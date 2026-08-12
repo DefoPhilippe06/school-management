2530d2e34c4d8ff8a4dbb9ff3bf741fc70e4326c
DefoPhilippe06 <dphilippejunior@gmail.com>
2026-08-12 07:40:16 +0100

    Configure email via environment variables for production

--- config/settings.py
+++ config/settings.py
@@ -143,12 +143,12 @@ CELERY_TIMEZONE = 'Africa/Lagos'
 # EMAIL CONFIGURATION (Gmail SMTP)
 # ======================
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'dphilippejunior@gmail.com'
-EMAIL_HOST_PASSWORD = 'zmjunopxfgtzlgqc'          # sans espaces
-DEFAULT_FROM_EMAIL = 'dphilippejunior@gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'dphilippejunior@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'zmjunopxfgtzlgqc')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
